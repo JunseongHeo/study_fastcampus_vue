d20ee67ef274dd7f5e972fca9669c5cd47204a36
허 준성 <oksk476@naver.com>
2023-07-23 21:54:23 +0900

    디렉티브 조건문과 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <!-- single file commponent -->
 <!-- HTML -->
 <template>
-  <h1 @click="increase">{{ count }}</h1>
+  <h1 @click="increase">
+    {{ count }}
+  </h1>
+  <!-- 디렉티브(v하이픈으로 제공하는 html 속성) > 조건문 -->
+  <div v-if="count > 4">
+    4보다 큽니다!
+  </div>
+  <!-- 디렉티브(v하이픈으로 제공하는 html 속성) > 반복문 -->
+  <ul>
+    <Fruit 
+      v-for="fruit in fruits" 
+      :key ="fruit"
+      :name ="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
 </template>
 
 <!-- JS -->
 <script>
+import Fruit from '~/components/Fruit'
+
 export default {
+  components : {
+    Fruit
+  },
   data : function () {
     return {
       count : 0
+      ,fruits : ['Apple', 'Banana', 'Cherry']
     }
   },
   methods : {
@@ -21,9 +42,14 @@ export default {
 </script>
 
 <!-- CSS -->
-<style>
+<style lang="scss">
   h1 {
     font-size : 50px;
     color : royalblue;
+  }
+  ul {
+    li {
+      font-size : 40px;
+    }
   }
 </style>
---
+++ src/components/Fruit.vue
@@ -0,0 +1,24 @@
+<template>
+ <li>
+    {{  name }}?!
+ </li>
+</template>
+
+<script>
+export default {
+    props : {
+        name : {
+            type : String
+            ,defalut : ''
+        }
+    }
+
+}
+</script>
+
+<!-- 유효범위 설정 scoped : 해당 컴포넌트 내부에만 영향 -->
+<style scoped lang="scss">
+    h1 {
+        color : red !important;
+    }
+</style>
