0c31a242b90c4626a78d5707683210ce5818d171
허 준성 <oksk476@naver.com>
2023-07-28 13:03:02 +0900

    템플릿문법 : v-once(랜더링이후 반응성x), v-html(일반 텍스트x html 적용)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <!-- single file commponent -->
 <!-- HTML -->
 <template>
-  <h1>
-    {{ count }}
+  <h1 
+    v-once
+    @click="add">
+    {{ msg }}
   </h1>
+  <h1 v-html="msg2"></h1>
   
 </template>
 
@@ -12,41 +15,19 @@
 export default {
   data() {
     return {
-      count : 2
+      msg : 'Hello world!',
+      msg2 : '<div style="color:red;">Hello world!</div>'
     }
-  }
-  // 컴포넌트 생성직전이라 많이 쓰이지 않음
-  ,beforeCreate() {
-    console.log('Before Create!', this.count);
-    console.log(document.querySelector('h1'));
-  }
-  // 컴포넌트 생성 직후에 실행되어 많이 쓰임. 컴포넌트에 대한 접근 등
-  ,created() {
-    console.log('Created!', this.count);
-    console.log(document.querySelector('h1'));
-  }
-  // html 연결 직전이라 많이 쓰이지 않음
-  ,beforeMount() {
-    console.log('Before Mount!');
-    console.log(document.querySelector('h1'));
-  }
-  // html 연결 직후라 많이 쓰임. dom 조작 등
-  ,mounted() {
-    console.log('Mounted!');
-    console.log(document.querySelector('h1'));
+  },
+  methods : {
+    add() {
+      this.msg += '!'
+    }
   }
 }
 </script>
 
 <!-- CSS -->
 <style lang="scss">
-  h1 {
-    font-size : 50px;
-    color : royalblue;
-  }
-  ul {
-    li {
-      font-size : 40px;
-    }
-  }
+
 </style>
